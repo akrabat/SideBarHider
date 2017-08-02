7deb3bfdf98515313af6659f59ec44227e80e7d1
bfelder <bfelder@users.noreply.github.com>
2017-08-02 20:07:01 +0200

    Corrected mistake.

--- HideSidebarWhenNotFocussed.py
+++ HideSidebarWhenNotFocussed.py
@@ -17,11 +17,6 @@ class ToggleSideBarCommand(sublime_plugin.TextCommand):
 class HideSidebarWhenNotFocussedListener(sublime_plugin.EventListener):
     def on_activated(self, view):
         if view and Vars.auto_hide:
-
-          
-class HideSidebarWhenNotFocussedListener(sublime_plugin.EventListener):
-    def on_activated(self, view):
-        if view:
             if len(view.window().views()) == 0:
                 # no open views, so show sidebar
                 view.window().set_sidebar_visible(True)
